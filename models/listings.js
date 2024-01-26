52f2fe7798a1e005b6f654283c587b6c0896ed86
Vicky <vikigiram@gmail.com>
2024-01-26 17:35:07 +0530

    Review added

--- models/listings.js
+++ models/listings.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
-
+const Review=require("./review.js")
 
 const listingSchema=new Schema({
     title:{
@@ -21,7 +21,18 @@ const listingSchema=new Schema({
     price:Number,
     location:String,
     country:String,
+    reviews:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: Review,
+        },
+    ]
+});
 
+listingSchema.post("findOneAndDelete",async (Listing)=>{
+    if(Listing){
+        await Review.deleteMany({_id: {$in: Listing.reviews}});
+    }
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 const Joi=require("joi");
 
-module.exports=Joi.object({
+const listingSchema=Joi.object({
     Listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
@@ -9,3 +9,14 @@ module.exports=Joi.object({
         country: Joi.string().required(),
     }).required().unknown(true)
 });
+
+
+const reviewSchema=Joi.object({
+    review: Joi.object({
+        rating: Joi.number().min(0).max(5).required(),
+        comment: Joi.string().required(),
+        date: Joi.date().optional()
+    }).required()
+});
+
+module.exports={listingSchema,reviewSchema};
